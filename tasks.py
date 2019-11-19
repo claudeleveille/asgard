d9179f677e4301bc5749eebe90e3d44090444875
Claude Léveillé <git@nerdin.space>
2019-11-18 19:50:03 -0500

    chore: added tasks.py

---
+++ tasks.py
@@ -0,0 +1,37 @@
+from functools import partial
+
+from invoke import task, run
+
+
+DOCKER_REPO = "claudeleveille/asgard"
+VERSION = "0.1.0rc1"
+
+
+sh = partial(run, echo=True)
+
+
+@task
+def fmt(c):
+    sh("black --verbose .")
+
+
+@task
+def check(c):
+    sh("black --check --verbose --diff .")
+    sh("pyflakes .")
+
+
+@task
+def test(c):
+    sh("pytest --verbose --ff --cov=asgard", pty=True)
+
+
+@task
+def package(c):
+    sh("pyinstaller --onefile --name asgard asgard/app.py")
+    sh(f"docker build --pull --tag {DOCKER_REPO}:{VERSION} .")
+
+
+@task
+def clean(c):
+    sh("git clean -dfx")
