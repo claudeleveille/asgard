6b69633bea11652f5d78551e40353b1f057fb509
Claude Léveillé <git@nerdin.space>
2019-11-18 19:48:58 -0500

    feat: added git.py

---
+++ asgard/git.py
@@ -0,0 +1,46 @@
+import subprocess
+import os
+import shutil
+from tempfile import mkdtemp
+
+
+class GitRepo:
+    def __init__(self, repo_path=None):
+        if repo_path is not None and os.path.exists(repo_path):
+            self.repo_path = repo_path
+        else:
+            self.repo_path = mkdtemp()
+        self.git("init")
+
+    def git(self, cmd, stdin_str=None):
+        return subprocess.run(
+            f"git {cmd}",
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            input=stdin_str,
+            cwd=self.repo_path,
+        )
+
+    def log(self):
+        res = self.git("log --pretty='%H'").stdout.decode()
+        if res:
+            rv = []
+            for line in res.split("\n"):
+                rv.append(
+                    {
+                        "hash": line,
+                        "message": self.git(f"show --no-patch --format='%B' {line}")
+                        .stdout.decode()
+                        .strip(),
+                    }
+                )
+            return tuple(rv)
+        else:
+            return ()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *exc):
+        shutil.rmtree(self.repo_path)
