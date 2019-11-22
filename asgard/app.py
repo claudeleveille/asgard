b02c0171deb0fa82b3ba49579fbe62c0a9f63399
Claude Léveillé <git@nerdin.space>
2019-11-21 22:39:35 -0500

    chore: fixed pyinstaller binary setup

---
+++ asgard/app.py
@@ -0,0 +1,25 @@
+import argparse
+import sys
+
+from asgard._version import __version__
+
+
+def main(args=sys.argv[1:]):
+    a = parse_args(args)
+
+    if a.version:
+        print(__version__)
+        sys.exit(0)
+
+
+def parse_args(args):
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--commit", action="store_true", default=False)
+    parser.add_argument("--tag", action="store_true", default=False)
+    parser.add_argument("--suffix-dot-suffix", action="store_true", default=False)
+    parser.add_argument("--suffix-dash-prefix", action="store_true", default=False)
+    parser.add_argument("--version", action="store_true", default=False)
+    parser.add_argument("--prerelease-suffix", default="")
+
+    return parser.parse_args(args)
